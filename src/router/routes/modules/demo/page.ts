a8fd0d8987abb8e2ecd1471bc44d9f96a2079dc7
hanrui <hi@hanrui.xyz>
2025-01-20 11:41:45 +0800

    refactor(layout): 优化页面布局和样式
    - 修改了 index.html 中的背景颜色
    - 更新了公共图片资源
    - 调整了 Table 组件中的钩子引用
    - 修改了路由模块中的页面名称

--- src/router/routes/modules/demo/page.ts
+++ src/router/routes/modules/demo/page.ts
@@ -167,7 +167,7 @@ const page: AppRouteModule = {
         },
         {
           path: '404',
-          name: 'PageNotFound',
+          name: 'PageNotFound-404',
           component: ExceptionPage,
           props: {
             status: ExceptionEnum.PAGE_NOT_FOUND,
