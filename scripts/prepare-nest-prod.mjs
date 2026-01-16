ac61d42cae50d8aadae6ba2fd289dacbf5f1e1bf
hanrui <hi@hanrui.xyz>
2026-01-16 17:05:14 +0800

    11

--- scripts/prepare-nest-prod.mjs
+++ scripts/prepare-nest-prod.mjs
@@ -37,7 +37,7 @@ async function main() {
       );
     },
   });
-  await fs.move(path.join(dir, 'node_modules'), path.join(dir, 'node_deps'), { overwrite: true });
+  await fs.move(path.join(DIST, 'node_modules'), path.join(DIST, 'node_deps'), { overwrite: true });
   console.log('✅ Nest files copied');
 
   // 3️⃣ 确保 dist 存在
